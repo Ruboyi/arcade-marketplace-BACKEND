45b6e67a5f6682690c800eaf6b3c47d0d41e54e5
nachhth <i.tharigen@gmail.com>
2021-12-06 10:34:43 +0100

    cleaning and uploading v1 database

---
+++ DATABASE-V1.sql
@@ -0,0 +1,92 @@
+DROP DATABASE IF EXISTS arcade;
+CREATE DATABASE arcade;
+USE arcade;
+
+CREATE TABLE users
+(
+  id_user INT NOT NULL auto_increment,
+  name VARCHAR(120) NOT NULL,
+  email VARCHAR(120) NOT NULL,
+  password VARCHAR(120) NOT NULL,
+  image VARCHAR(120),
+  createdAt DATE NOT NULL,
+  VerifiedAt DATE,
+  updatedAt DATE,
+  verificationCode VARCHAR(255),
+  role enum('admin','user') default 'user' NOT NULL,
+  PRIMARY KEY (id_user),
+  UNIQUE (email)
+);
+
+CREATE TABLE products
+(
+  id_productos INT NOT NULL auto_increment,
+  titulo VARCHAR(120) NOT NULL,
+  descripcion VARCHAR(255) NOT NULL,
+  precio INT NOT NULL,
+  localidad VARCHAR(120) NOT NULL,
+  createdAt DATE NOT NULL,
+  updatedAt DATE,
+  timesVisited INT NOT NULL,
+  id_user INT NOT NULL,
+  PRIMARY KEY (id_productos),
+  FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE reviews
+(
+  id_review INT NOT NULL auto_increment,
+  comment VARCHAR(255),
+  createdAt DATE NOT NULL,
+  rating INT NOT NULL,
+  user_valorador INT NOT NULL,
+  id_user INT NOT NULL,
+  PRIMARY KEY (id_review),
+  FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE product_images
+(
+  id_image INT NOT NULL auto_increment,
+  name_image VARCHAR(255) NOT NULL,
+  principal boolean NOT NULL default false,
+  id_productos INT NOT NULL,
+  PRIMARY KEY (id_image),
+  FOREIGN KEY (id_productos) REFERENCES products(id_productos)
+);
+
+CREATE TABLE favorites
+(
+  id_favorite INT NOT NULL auto_increment,
+  id_user INT NOT NULL,
+  id_productos INT NOT NULL,
+  PRIMARY KEY (id_favorite),
+  FOREIGN KEY (id_user) REFERENCES users(id_user),
+  FOREIGN KEY (id_productos) REFERENCES products(id_productos),
+  UNIQUE (id_user, id_productos)
+);
+
+CREATE TABLE reservas
+(
+  id_reserva INT NOT NULL auto_increment,
+  id_productos INT NOT NULL,
+  id_user INT NOT NULL,
+  PRIMARY KEY (id_reserva),
+  FOREIGN KEY (id_productos) REFERENCES products(id_productos),
+  FOREIGN KEY (id_user) REFERENCES users(id_user),
+  UNIQUE (id_productos, id_user)
+);
+
+CREATE TABLE vendidos
+(
+  id_venta INT NOT NULL auto_increment,
+  date DATE NOT NULL,
+  id_user INT NOT NULL,
+  id_productos INT NOT NULL,
+  PRIMARY KEY (id_venta),
+  FOREIGN KEY (id_user) REFERENCES users(id_user),
+  FOREIGN KEY (id_productos) REFERENCES products(id_productos),
+  UNIQUE (id_user, id_productos)
+);
+
+
