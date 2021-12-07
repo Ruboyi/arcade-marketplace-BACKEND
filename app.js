31b6044b9eaa471f6f7704c3d0a9c02f19bc6bc2
nachhth <i.tharigen@gmail.com>
2021-12-07 19:30:06 +0100

    added first lines of code on app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+'use strict';
+
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const fileUpload = require('express-fileupload');
+const { PORT } = process.env;
+
+app.use(fileUpload());
+// Para recibir datos como JSON en el BODY
+app.use(express.json());
+// CORS - Para dar permisos de acceso a otras url's
+app.use(cors());
+// To serve static files => Archivos públicos.
+app.use(express.static('public'));
+
+const usersRouter = require('./app/routes/user-routes');
+
+// ESTO ESTA COMENTADO PORQUE FALTA CREAR EL ENDPOINT ENTERO
+// app.use('/api/v1/users/', usersRouter);
+
+app.listen(PORT, () => console.log('Running', PORT));
---
+++ app/routes/user-routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+// URL's PÚBLICAS
+
+router.route('/').get();
+
+// URL's PRIVADAS
