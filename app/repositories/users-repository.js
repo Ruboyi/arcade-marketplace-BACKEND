85b629d2e4ca179f817468cf488d16d7bb34dbdf
nachhth <i.tharigen@gmail.com>
2021-12-10 17:38:00 +0100

    validateUser endpoint

--- app/repositories/users-repository.js
+++ app/repositories/users-repository.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-const getPool = require("../infrastructure/database-infrastructure");
+const getPool = require('../infrastructure/database-infrastructure');
 
 async function findUserById(userId) {
   const pool = await getPool();
-  const sql = "SELECT * FROM users WHERE idUser = ?";
+  const sql = 'SELECT * FROM users WHERE idUser = ?';
   const [user] = await pool.query(sql, userId);
   return user[0];
 }
@@ -19,25 +19,41 @@ async function createUser(user) {
   `;
   const { nameUser, email, passwordHash, phone, verificationCode } = user;
   const now = new Date();
-  const [created] = await pool.query(sql, [
-    nameUser,
-    email,
-    passwordHash,
-    phone,
-    now,
-    verificationCode,
-    "user",
-  ]);
+  const [created] = await pool.query(sql, [nameUser, email, passwordHash, phone, now, verificationCode, 'user']);
 
   return created.insertId;
 }
 
 async function findUserByEmail(email) {
   const pool = await getPool();
-  const sql =
-    "SELECT idUser, nameUser, email, role, password, verifiedAt FROM users WHERE email = ?";
+  const sql = 'SELECT idUser, nameUser, email, role, password, verifiedAt FROM users WHERE email = ?';
   const [user] = await pool.query(sql, email);
 
+  return user[0];
+}
+
+async function activateUser(verificationCode) {
+  const now = new Date();
+  const pool = await getPool();
+  const sql = `
+  UPDATE users
+  SET verifiedAt = ?
+  WHERE verificationCode = ?
+  AND verifiedAt IS NULL
+  `;
+
+  const [result] = await pool.query(sql, [now, verificationCode]);
+
+  return result.affectedRows === 1;
+}
+
+async function getUserByVerificationCode(code) {
+  const pool = await getPool();
+  const sql = `
+  SELECT nameUser, email FROM users
+  WHERE verificationCode = ?    
+  `;
+  const [user] = await pool.query(sql, code);
   return user[0];
 }
 
@@ -45,4 +61,6 @@ module.exports = {
   findUserById,
   createUser,
   findUserByEmail,
+  activateUser,
+  getUserByVerificationCode,
 };
--- app/routes/users-routes.js
+++ app/routes/users-routes.js
@@ -1,16 +1,19 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const getUserById = require("../controllers/users/get-user-by-id-controller");
-const registerUser = require("../controllers/users/register-user-controller");
+const express = require('express');
+const { route } = require('express/lib/application');
+const validateUser = require('../controllers/users/activate-user-controller');
+const getUserById = require('../controllers/users/get-user-by-id-controller');
+const registerUser = require('../controllers/users/register-user-controller');
 const router = express.Router();
 // require a los controllers, por ejemplo:
 // const nombreFuncion = require('../')
 
 // URL's PÚBLICAS, por ejemplo:
 // router.route('/').get(nombreFuncion);
-router.route("/:idUser").get(getUserById);
-router.route("/").post(registerUser);
+router.route('/').post(registerUser);
+router.route('/activation').get(validateUser);
+router.route('/:idUser').get(getUserById);
 // URL's PRIVADAS (aquellas que tienen la función validateAuth por delante), por ejemplo:
 // router.route('/').all(validateAuth).delete(nombreFuncion)
 
