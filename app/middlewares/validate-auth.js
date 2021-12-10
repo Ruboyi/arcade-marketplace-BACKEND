0222dd421da72aa032b6326cdb7474eb0f42f5e7
Ruboyi <rubo2093@gmail.com>
2021-12-10 13:55:25 +0100

    añadido validate-auth

---
+++ app/middlewares/validate-auth.js
@@ -0,0 +1,32 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+const createJsonError = require('../errors/create-json-error');
+const throwJsonError = require('../errors/throw-json-error');
+const { JWT_SECRET } = process.env;
+
+function extractAccessToken(headers) {
+  const { authorization } = headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throwJsonError(403, 'Autorización requerida');
+  }
+
+  return authorization.split(" ")[1];
+  // return authorization.slice(7, authorization.length);
+}
+
+function validateAuth(req, res, next) {
+  try {
+    const { headers } = req;
+    const token = extractAccessToken(headers);
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+
+    const { id, name, role } = decodedToken;
+    req.auth = { id, name, role };
+
+    next();
+  } catch (error) {
+    createJsonError(error, res);
+  }
+}
+
+module.exports = validateAuth;
