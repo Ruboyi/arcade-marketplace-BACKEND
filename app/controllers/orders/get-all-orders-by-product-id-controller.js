4a22e700593021a31cf51fc338b20b9a074b626b
RicardoMejiaV <rdesign777@gmail.com>
2021-12-10 19:56:48 +0100

    creamos getAllOrdersByProductId endpoint

---
+++ app/controllers/orders/get-all-orders-by-product-id-controller.js
@@ -0,0 +1,25 @@
+'use strict';
+const Joi = require('joi');
+const createJsonError = require('../../errors/create-json-error');
+const throwJsonError = require('../../errors/throw-json-error');
+const { findAllOrdersByProductId } = require('../../repositories/orders-repository');
+
+const schema = Joi.number().integer().positive().required();
+
+async function getAllOrdersByProductId(req, res) {
+  try {
+    const {idUser} = req.auth;
+    const {idProduct} = req.params;
+    await schema.validateAsync(idProduct);
+    const orders = await findAllOrdersByProductId(idProduct);
+    if (orders.length === 0) {
+        throwJsonError(400, 'No hay ordenes de compra')
+    };
+    res.status(200);
+    res.send({data: orders});
+  } catch (error) {
+    createJsonError(error, res);
+  }
+}
+
+module.exports = getAllOrdersByProductId;
--- app/repositories/orders-repository.js
+++ app/repositories/orders-repository.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const getPool = require('../infrastructure/database-infrastructure');
+
+async function findAllOrdersByProductId(idProduct) {
+    const pool = await getPool();
+    const sql = `SELECT * FROM orders WHERE idProduct = ?`;
+    const [orders] = await pool.query(sql, idProduct);
+    return orders;
+}
+
+module.exports = {
+    findAllOrdersByProductId,
+  };
