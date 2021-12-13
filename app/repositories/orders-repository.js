8a8ce0a17a323c18fab74cafa69f3760ac98d0fb
RicardoMejiaV <rdesign777@gmail.com>
2021-12-13 21:01:50 +0100

    agregamos getAllOrdersByUserBuyer

--- app/repositories/orders-repository.js
+++ app/repositories/orders-repository.js
@@ -4,11 +4,19 @@ const getPool = require('../infrastructure/database-infrastructure');
 
 async function findAllOrdersByProductId(idProduct) {
     const pool = await getPool();
-    const sql = `SELECT * FROM orders WHERE idProduct = ?`;
+    const sql = 'SELECT * FROM orders WHERE idProduct = ?';
     const [orders] = await pool.query(sql, idProduct);
     return orders;
-}
+};
+
+async function findAllOrdersByUserBuyerId(idUserBuyer) {
+    const pool = await getPool();
+    const sql = 'SELECT * FROM orders WHERE idUserBuyer = ?';
+    const [orders] = await pool.query(sql, idUserBuyer);
+    return orders;
+};
 
 module.exports = {
     findAllOrdersByProductId,
+    findAllOrdersByUserBuyerId
   };
