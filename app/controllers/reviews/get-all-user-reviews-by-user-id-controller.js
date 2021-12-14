d3aedba068dd0f9304fa89cd9c1cb46707344224
Salva <salvadorgarciafan@gmail.com>
2021-12-14 17:53:00 +0100

    Añadiendo getAllUserReviewsByUserIdController

---
+++ app/controllers/reviews/get-all-user-reviews-by-user-id-controller.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Joi = require("joi");
+const createJsonError = require("../../errors/create-json-error");
+const throwJsonError = require("../../errors/throw-json-error");
+const {
+  findAllUserReviewsById,
+} = require("../../repositories/reviews-repository");
+const { findUserById } = require("../../repositories/users-repository");
+const schema = Joi.number().positive().required();
+
+async function getAllUserReviewsByUserId(req, res) {
+  try {
+    const { idUser } = req.params;
+    await schema.validateAsync(idUser);
+    const user = await findUserById(idUser);
+
+    if (!user) {
+      throwJsonError(400, "El usuario no existe");
+    }
+    const reviews = await findAllUserReviewsById(idUser);
+    res.status(200);
+    res.send({ data: reviews });
+  } catch (error) {
+    createJsonError(error, res);
+  }
+}
+
+module.exports = getAllUserReviewsByUserId;
--- app/routes/reviews-routes.js
+++ app/routes/reviews-routes.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const express = require("express");
+const getAllUserReviewsByUserId = require("../controllers/reviews/get-all-user-reviews-by-user-id-controller");
 const validateAuth = require("../middlewares/validate-auth");
 const router = express.Router();
 
 //aqui van las rutas
+router.route("/:idUser").get(getAllUserReviewsByUserId);
 
 module.exports = router;
