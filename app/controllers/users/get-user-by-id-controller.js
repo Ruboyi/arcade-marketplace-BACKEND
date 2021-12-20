2ba22a1a9eea7c6dc655059ca63f4e0f61d235fd
nachhth <i.tharigen@gmail.com>
2021-12-20 10:43:27 +0100

    find image by Product id tested and working, added verfiedAt on getUserById

--- app/controllers/users/get-user-by-id-controller.js
+++ app/controllers/users/get-user-by-id-controller.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-const Joi = require("joi");
-const createJsonError = require("../../errors/create-json-error");
-const throwJsonError = require("../../errors/throw-json-error");
-const { findUserById } = require("../../repositories/users-repository");
+const Joi = require('joi');
+const createJsonError = require('../../errors/create-json-error');
+const throwJsonError = require('../../errors/throw-json-error');
+const { findUserById } = require('../../repositories/users-repository');
 const schema = Joi.number().integer().positive().required();
 
 async function getUserById(req, res) {
@@ -12,11 +12,11 @@ async function getUserById(req, res) {
     await schema.validateAsync(idUser);
     const user = await findUserById(idUser);
     if (!user) {
-      throwJsonError(400, "El usuario no existe");
+      throwJsonError(400, 'El usuario no existe');
     }
-    const { nameUser, email, image, createdAt } = user;
+    const { nameUser, email, image, createdAt, verifiedAt } = user;
     res.status(200);
-    res.send({ nameUser, email, image, createdAt });
+    res.send({ nameUser, email, image, createdAt, verifiedAt });
   } catch (error) {
     createJsonError(error, res);
   }
