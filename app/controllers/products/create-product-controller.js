808c77f4394cf5d2f64853b160aaf427ecddfc26
Ruboyi <rubo2093@gmail.com>
2022-01-22 22:45:21 +0100

    Devolviendo productId al crear product

--- app/controllers/products/create-product-controller.js
+++ app/controllers/products/create-product-controller.js
@@ -1,16 +1,21 @@
-'use strict';
+"use strict";
 
-const Joi = require('joi');
-const createJsonError = require('../../errors/create-json-error');
-const { addProduct } = require('../../repositories/products-repository');
+const Joi = require("joi");
+const createJsonError = require("../../errors/create-json-error");
+const { addProduct } = require("../../repositories/products-repository");
 
 const schema = Joi.object().keys({
   title: Joi.string().min(3).max(120).required(),
   description: Joi.string().min(10).max(255).required(),
   price: Joi.number().positive().precision(2).required(),
   location: Joi.string().min(3).max(120).required(),
-  category: Joi.string().valid('consolas', 'videojuegos', 'accesorios', 'arcades'),
-  state: Joi.string().valid('nuevo', 'seminuevo', 'usado'),
+  category: Joi.string().valid(
+    "consolas",
+    "videojuegos",
+    "accesorios",
+    "arcades"
+  ),
+  state: Joi.string().valid("nuevo", "seminuevo", "usado"),
   idUser: Joi.number().integer().positive().required(),
 });
 
@@ -23,7 +28,10 @@ async function createProduct(req, res) {
     const productId = await addProduct(product);
 
     res.status(201);
-    res.send({ message: `Producto ${productId} creado correctamente` });
+    res.send({
+      message: `Producto ${productId} creado correctamente`,
+      productId: `${productId}`,
+    });
   } catch (error) {
     createJsonError(error, res);
   }
