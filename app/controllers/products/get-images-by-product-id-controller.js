85edf0b75d6cc3a5a3755f1cf7d0f54ffad908b5
nachhth <i.tharigen@gmail.com>
2022-01-23 19:02:38 +0100

    modified get images by product id controller

--- app/controllers/products/get-images-by-product-id-controller.js
+++ app/controllers/products/get-images-by-product-id-controller.js
@@ -2,28 +2,45 @@
 const Joi = require('joi');
 const createJsonError = require('../../errors/create-json-error');
 const throwJsonError = require('../../errors/throw-json-error');
-const { findImagesByProductId } = require('../../repositories/product-images-repository');
-const { findProductByidProduct } = require('../../repositories/products-repository');
+const {
+  findImagesByProductId
+} = require('../../repositories/product-images-repository');
+const {
+  findProductByidProduct
+} = require('../../repositories/products-repository');
 
 const schema = Joi.number().integer().positive().required();
 
+const { HTTP_SERVER, PATH_PRODUCTS_IMAGE } = process.env;
+
 async function getImagesByProductId(req, res) {
-    try {
-      const {idProduct} = req.params;
-      await schema.validateAsync(idProduct);
-      const product = await findProductByidProduct(idProduct);
+  try {
+    const { idProduct } = req.params;
+    await schema.validateAsync(idProduct);
+    const product = await findProductByidProduct(idProduct);
     if (!product) {
       throwJsonError(400, 'No existe el producto');
     }
     const images = await findImagesByProductId(idProduct);
     if (images.length === 0) {
-    throwJsonError(400, 'No hay imágenes para este producto');
-      }
-      res.status(200);
-      res.send({ data: images });
-    } catch (error) {
-      createJsonError(error, res);
+      throwJsonError(400, 'No hay imágenes para este producto');
     }
+
+    const mapperProductImagesWithURL = images.map((imageData) => {
+      const { nameImage, mainImage } = imageData;
+      const imgURL = `${HTTP_SERVER}/${PATH_PRODUCTS_IMAGE}/${idProduct}/${nameImage}`;
+
+      return {
+        image: imgURL,
+        mainImage: mainImage
+      };
+    });
+
+    res.status(200);
+    res.send({ data: mapperProductImagesWithURL });
+  } catch (error) {
+    createJsonError(error, res);
   }
-  
-  module.exports = getImagesByProductId;
+}
+
+module.exports = getImagesByProductId;
