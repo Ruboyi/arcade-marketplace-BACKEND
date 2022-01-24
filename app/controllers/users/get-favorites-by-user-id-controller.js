0ed755563da84a1198cb7f7a01424043599d84d9
nachhth <i.tharigen@gmail.com>
2022-01-24 21:20:46 +0100

    favoritesImages fixed

--- app/controllers/users/get-favorites-by-user-id-controller.js
+++ app/controllers/users/get-favorites-by-user-id-controller.js
@@ -1,12 +1,15 @@
-"use strict";
+'use strict';
 
-const Joi = require("joi");
-const createJsonError = require("../../errors/create-json-error");
-const throwJsonError = require("../../errors/throw-json-error");
+const Joi = require('joi');
+const createJsonError = require('../../errors/create-json-error');
+const throwJsonError = require('../../errors/throw-json-error');
+const {
+  findImagesByProductId
+} = require('../../repositories/product-images-repository');
 const {
   findUserById,
-  findFavoritesByUserId,
-} = require("../../repositories/users-repository");
+  findFavoritesByUserId
+} = require('../../repositories/users-repository');
 
 const schema = Joi.number().positive().required();
 
@@ -16,12 +19,28 @@ async function getFavoritesByUserId(req, res) {
     await schema.validateAsync(idUser);
     const user = await findUserById(idUser);
     if (!user) {
-      throwJsonError(400, "El usuario no existe");
+      throwJsonError(400, 'El usuario no existe');
     }
-    let favorites = await findFavoritesByUserId(idUser);
+    const favorites = await findFavoritesByUserId(idUser);
+
+    const images = [];
+
+    for (const product of favorites) {
+      images.push(await findImagesByProductId(product.idProduct));
+    }
+
+    const mapperProductWithImages = favorites.map((productData, index) => {
+      const { idProduct, ...rest } = productData;
+
+      return {
+        idProduct,
+        ...rest,
+        images: images[index]
+      };
+    });
 
     res.status(200);
-    res.send({ data: favorites });
+    res.send({ data: mapperProductWithImages });
   } catch (error) {
     createJsonError(error, res);
   }
