e5c16f821691c5a30c6b5ac2a9cb209b12a2c8f5
nachhth <i.tharigen@gmail.com>
2022-01-24 17:51:03 +0100

    anhadido al  array de productos la url de la imagen

--- app/controllers/products/get-all-products-controller.js
+++ app/controllers/products/get-all-products-controller.js
@@ -2,7 +2,14 @@
 
 const createJsonError = require('../../errors/create-json-error');
 const throwJsonError = require('../../errors/throw-json-error');
-const { findAllProducts } = require('../../repositories/products-repository');
+const {
+  findImagesByProductId
+} = require('../../repositories/product-images-repository');
+const {
+  findAllProducts
+} = require('../../repositories/products-repository');
+
+const { HTTP_SERVER, PATH_PRODUCTS_IMAGE } = process.env;
 
 async function getAllProducts(req, res) {
   try {
@@ -10,8 +17,27 @@ async function getAllProducts(req, res) {
     if (products.length === 0) {
       throwJsonError(400, 'Productos no encontrados');
     }
+
+    const images = [];
+
+    for (const product of products) {
+      images.push(await findImagesByProductId(product.idProduct));
+    }
+
+    const mapperProductWithIMAGES = products.map((productData, index) => {
+      const { idProduct, ...rest } = productData;
+      const { nameImage } = images[index][0]; // TODO - AÑADIR FUNCION PARA QUE RECOJA LA MAIN IMAGE
+      const imgURL = `${HTTP_SERVER}/${PATH_PRODUCTS_IMAGE}/${idProduct}/${nameImage}`;
+
+      return {
+        idProduct,
+        ...rest,
+        image: imgURL
+      };
+    });
+
     res.status(200);
-    res.send({ data: products });
+    res.send({ data: mapperProductWithIMAGES });
   } catch (error) {
     createJsonError(error, res);
   }
