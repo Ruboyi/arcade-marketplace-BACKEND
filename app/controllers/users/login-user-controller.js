6bf629e9f15b9d95f8a18b9f7c247d6abd827c53
Ruboyi <rubo2093@gmail.com>
2022-02-16 14:07:31 +0100

    añadiendo role al login

--- app/controllers/users/login-user-controller.js
+++ app/controllers/users/login-user-controller.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const Joi = require('joi');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const createJsonError = require('../../errors/create-json-error');
-const throwJsonError = require('../../errors/throw-json-error');
-const { findUserByEmail } = require('../../repositories/users-repository');
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const createJsonError = require("../../errors/create-json-error");
+const throwJsonError = require("../../errors/throw-json-error");
+const { findUserByEmail } = require("../../repositories/users-repository");
 const schema = Joi.object().keys({
   email: Joi.string().email().required(),
-  password: Joi.string().min(4).max(20).required()
+  password: Joi.string().min(4).max(20).required(),
 });
 
 async function loginUser(req, res) {
@@ -19,39 +19,28 @@ async function loginUser(req, res) {
     const { email, password } = body;
     const user = await findUserByEmail(email);
     if (!user) {
-      throwJsonError(
-        403,
-        'No existe un usuario con ese email y/o password'
-      );
+      throwJsonError(403, "No existe un usuario con ese email y/o password");
     }
-    const {
-      idUser,
-      nameUser,
-      role,
-      password: passwordHash,
-      verifiedAt
-    } = user;
+    const { idUser, nameUser, role, password: passwordHash, verifiedAt } = user;
     const isValidPassword = await bcrypt.compare(password, passwordHash);
     if (!isValidPassword) {
-      throwJsonError(
-        403,
-        'No existe un usuario con ese email y/o password'
-      );
+      throwJsonError(403, "No existe un usuario con ese email y/o password");
     }
     if (!verifiedAt) {
       throwJsonError(
         401,
-        'Verifique su cuenta poder acceder a nuestros servicios'
+        "Verifique su cuenta poder acceder a nuestros servicios"
       );
     }
     const { JWT_SECRET } = process.env;
     const tokenPayload = { idUser, nameUser, role };
     const token = jwt.sign(tokenPayload, JWT_SECRET, {
-      expiresIn: '365 days'
+      expiresIn: "365 days",
     });
     const response = {
       accessToken: token,
-      expiresIn: '365 days'
+      expiresIn: "365 days",
+      role: role,
     };
 
     res.status(200);
